5bc723c432521d0cc86e5add3b813ba14aee8cec
Marco A. Rojas <contact@marcorojas.me>
2018-05-31 11:48:38 -0500

    Starting OpenGl

---
+++ practica1/basic.cpp
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <GLUT/glut.h>
+
+// Rutina de dibujado (display)
+void drawScene(void)
+{
+    glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0, 0.0, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex3f(20.0, 20.0, 0.0);
+    glVertex3f(80.0, 20.0, 0.0);
+    glVertex3f(80.0, 80.0, 0.0);
+    glVertex3f(20.0, 80.0, 0.0);
+    glEnd();
+    glFlush();
+}
+// Rutina de inicialización
+void setup(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+}
+
+// OpenGL window reshape routine.
+void resize(int w, int h)
+{
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+void keyInput(unsigned char key, int x, int y)
+{
+    switch (key)
+    { // Presiona ESC para EXIT
+    case 27:
+        exit(0);
+        break;
+    default:
+        break;
+    }
+}
+
+int main(int argc, char **argv)
+{
+glutInit(&argc, argv);
+glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+glutInitWindowSize(500, 500);
+glutInitWindowPosition(100, 100);
+glutCreateWindow("Cuadrado");
+setup();
+glutDisplayFunc(drawScene);
+glutReshapeFunc(resize);
+glutKeyboardFunc(keyInput);
+glutMainLoop();
+return 0;
+}
---
+++ practica1/config.cpp
@@ -0,0 +1,33 @@
+#include <GLUT/glut.h>
+#include <stdlib.h>
+
+void dibujar();
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(800, 500);
+    glutInitWindowPosition(100, 100);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH);
+	glutCreateWindow(argv[0]);
+    glutDisplayFunc(dibujar);
+    glutSetWindowTitle("Bad King 1.0 👑");
+
+    glutMainLoop();
+
+	return 0;
+}
+
+void dibujar(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(-1, -1, 0.0);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.0, 1, 0.0);
+    glutSwapBuffers();
+    glEnd();
+}
