fa5c6cc185a3eec86c7d965066a4181d7a123d5a
Marco A. Rojas <contact@marcorojas.me>
2018-06-20 18:29:27 -0500

    Practica 5: Using different types of lights and menus

--- practica5/05_Iluminacion3D.cpp
+++ practica5/05_Iluminacion3D.cpp
@@ -1,25 +1,70 @@
-//JUAN CARLOS CONDE RAM�REZ
-//OBJETOS 3D - ILUMINACION
 #include <GLUT/glut.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
 
 //Matriz de colores
-float colores[4][3] =
+float colores[9][3] =
 	{
 		{1.0, 1.0, 1.0}, //0 Blanco
 		{1.0, 0.0, 0.0}, //1 Rojo
 		{0.0, 1.0, 0.0}, //2 Verde
 		{0.0, 0.0, 1.0}, //3 Azul
-		{0.0, 0.0, 0.0}, //4 Negro
-		{1.0, 1.0, 0.0}, //5 Amarillo
-		{1.0, 0.0, 1.0}, //6 Magenta
-		{0.0, 1.0, 1.0}, //7 Cian
-		{0.8, 0.8, 0.8}  //8 Gris
+		{1.0, 1.0, 0.0}, //4 Amarillo
+		{1.0, 0.0, 1.0}, //5 Magneta
+		{0.0, 1.0, 1.0}, //6 Cian
+		{0.8, 0.8, 0.8}, //7 Gris
+		{0.0, 0.0, 0.0}, //8 Negro
 };
 
-int color = 2;  // color de FIGURA
-int colorf = 0; // color de FONDO
-int colorl = 3; // color de LUZ
+float *getColor(string color)
+{
+	if (color.compare("white") == 0)
+	{
+		return colores[0];
+	}
+	else if (color.compare("red") == 0)
+	{
+		return colores[1];
+	}
+	else if (color.compare("green") == 0)
+	{
+		return colores[2];
+	}
+	else if (color.compare("blue") == 0)
+	{
+		return colores[3];
+	}
+	else if (color.compare("yellow") == 0)
+	{
+		return colores[4];
+	}
+	else if (color.compare("magenta") == 0)
+	{
+		return colores[5];
+	}
+	else if (color.compare("cyan") == 0)
+	{
+		return colores[6];
+	}
+	else if (color.compare("gray") == 0)
+	{
+		return colores[7];
+	}
+	else if (color.compare("black") == 0)
+	{
+		return colores[8];
+	}
+}
+int window;
+
+string color = "white";   // color de FIGURA
+string colorf = "black"; // color de FONDO
+int colorl = 3;	// color de LUZ
+string coloresp = "gray";
 
 //variables globales para la rotacion
 float alpha, beta;
@@ -52,30 +97,32 @@ typedef enum
 	C9,
 	E1,
 	E2,
+	E3,
 	D1,
 	D2,
+	D3,
 	S1,
 	S2
 } opcionMenu;
 
 void luzAmbiental(void)
 {
-	GLfloat l_difusa[] = {0.5f, 0.5f, 0.5f, 0.0f};
-	GLfloat l_especular[] = {0.5f, 0.5f, 0.5f, 0.0f};
+	GLfloat l_difusa[] = {getColor(color)[0], getColor(color)[1], getColor(color)[2], 1.0f};
+	GLfloat l_especular[] = {getColor(coloresp)[0], getColor(coloresp)[1], getColor(coloresp)[2], 1.0f};
+	GLfloat l_a_color[] = {getColor(coloresp)[0], getColor(coloresp)[1], getColor(coloresp)[2], 1.0f};
 	GLfloat l_posicion[] = {0.0, 0.0, 5.0, 1.0};
-	GLfloat l_a_color[] = {0.5f, 0.5f, 0.5f, 0.0f};
 
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, l_difusa);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, l_difusa);
-	glLightfv(GL_LIGHT0, GL_POSITION, l_posicion);
-	//glLightfv(GL_LIGHT0, GL_AMBIENT, l_a_color);
+	//glLightfv(GL_LIGHT0, GL_POSITION, l_posicion);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, l_a_color);
 	glEnable(GL_LIGHT0);
 }
 
 void luzUno(void)
 {
-	GLfloat l_difusa[] = {colores[2][0], colores[2][1], colores[2][2], 0.0f};
-	GLfloat l_especular[] = {colores[2][0], colores[2][1], colores[2][2], 0.0f};
+	GLfloat l_difusa[] = {getColor("green")[0], getColor("green")[1], getColor("green")[2], 0.0f};
+	GLfloat l_especular[] = {getColor("gray")[0], getColor("gray")[1], getColor("gray")[2], 0.0f};
 	GLfloat l_posicion[] = {0.0f, 5.0f, 0.0f, 1.0f};
 
 	glLightfv(GL_LIGHT1, GL_DIFFUSE, l_difusa);
@@ -84,22 +131,32 @@ void luzUno(void)
 	//glEnable (GL_LIGHT1);
 }
 
+void luzDos(void)
+{
+	GLfloat l_difusa[] = {getColor("blue")[0], getColor("blue")[1], getColor("blue")[2], 0.0f};
+	GLfloat l_especular[] = {getColor("cyan")[0], getColor("cyan")[1], getColor("cyan")[2], 0.0f};
+	GLfloat l_posicion[] = {0.0f, -5.0f, 0.0f, 0.0f};
+
+	glLightfv(GL_LIGHT2, GL_DIFFUSE, l_difusa);
+	glLightfv(GL_LIGHT2, GL_SPECULAR, l_difusa);
+	glLightfv(GL_LIGHT2, GL_POSITION, l_posicion);
+}
+
 void luzTres(void)
 {
-	GLfloat l_difusa[] = {colores[0][0], colores[0][1], colores[0][2], 0.0f};
-	GLfloat l_especular[] = {colores[0][0], colores[0][1], colores[0][2], 0.0f};
+	GLfloat l_difusa[] = {getColor("yellow")[0], getColor("yellow")[1], getColor("yellow")[2], 1.0f};
+	GLfloat l_especular[] = {getColor("red")[0], getColor("red")[1], getColor("red")[2], 1.0f };
 
-	GLfloat l_posicion[] = {0.0, 0.0, 0.0, 1.0};
-	GLfloat spot_direccion[] = {0.0, 0.0, 0.0, 0.0};
+	GLfloat l_position[] = {0.0f, 0.0f, 6.0f, 1.0f};
+	GLfloat spot_direction[] = {0.0f, 0.0f, -1.0f, 1.0f};
 
 	glLightfv(GL_LIGHT3, GL_DIFFUSE, l_difusa);
 	glLightfv(GL_LIGHT3, GL_SPECULAR, l_difusa);
-	glLightfv(GL_LIGHT3, GL_POSITION, l_posicion);
+	glLightfv(GL_LIGHT3, GL_POSITION, l_position);
 
 	glLightf(GL_LIGHT3, GL_SPOT_CUTOFF, 3.0);
 	glLightf(GL_LIGHT3, GL_SPOT_EXPONENT, 2.0);
-	glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, spot_direccion);
-	//glEnable (GL_LIGHT3);
+	glLightfv(GL_LIGHT3, GL_SPOT_DIRECTION, spot_direction);
 }
 
 void init(void)
@@ -111,10 +168,8 @@ void init(void)
 
 void display(void)
 {
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-
+	glClearColor(getColor(colorf)[0], getColor(colorf)[1], getColor(colorf)[2], 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
@@ -138,12 +193,36 @@ void display(void)
 	glRotatef(alpha, 1.0f, 0.0f, 0.0f);
 	glRotatef(beta, 0.0f, 1.0f, 0.0f);
 
-	glColor3f(1.0f, 1.0f, 1.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
 
 	switch (opc)
 	{
 	case 1:
-		glutSolidCube(0.5);
+		glutSolidCube(0.8);
+		break;
+	case 2:
+		glutSolidCone(1, 2, 20, 20);
+		break;
+	case 3:
+		glutSolidSphere(1, 20, 20);
+		break;
+	case 4:
+		glutSolidTeapot(0.8);
+		break;
+	case 5:
+		glutSolidTorus(0.4, 0.8, 10, 40);
+		break;
+	case 6:
+		glutSolidIcosahedron();
+		break;
+	case 7:
+		glutSolidDodecahedron();
+		break;
+	case 8:
+		glutSolidOctahedron();
+		break;
+	case 9:
+		glutSolidTetrahedron();
 		break;
 	}
 
@@ -180,16 +259,97 @@ void menuFiguras(int opcion)
 	case F1:
 		opc = 1;
 		break;
-	case C1:
-		colorf = 0;
+
+	case F2:
+		opc = 2;
 		break;
+	case F3:
+		opc = 3;
+		break;
+	case F4:
+		opc = 4;
+		break;
+	case F5:
+		opc = 5;
+		break;
+	case F6:
+		opc = 6;
+		break;
+	case F7:
+		opc = 7;
+		break;
+	case F8:
+		opc = 8;
+		break;
+	case F9:
+		opc = 9;
+		break;
+
+	case C1:
+		colorf = "white";
+		break;
+	case C2:
+		colorf = "black";
+		break;
+
+	case C3:
+		color = "red";
+		break;
+
+	case C4:
+		color = "green";
+		break;
+
+	case C5:
+		color = "blue";
+		break;
+
+	case C6:
+		color = "yellow";
+		break;
+
+	case C7:
+		color = "magenta";
+		break;
+
+	case C8:
+		color = "cyan";
+		break;
+
+	case C9:
+		color = "gray";
+		break;
+
 	case E1:
 		glEnable(GL_LIGHT1);
+		glutChangeToMenuEntry(1, "Desactivar luz 1", D1);
+		break;
+	case E2:
+		glEnable(GL_LIGHT2);
+		glutChangeToMenuEntry(2, "Desactivar luz 2", D2);
+		break;
+	case E3:
+		glEnable(GL_LIGHT3);
+		glutChangeToMenuEntry(3, "Desactivar luz 3", D3);
+		break;
+
+	case D1:
+		glDisable(GL_LIGHT1);
+		glutChangeToMenuEntry(1, "Activar luz 1", E1);
+		break;
+	case D2:
+		glDisable(GL_LIGHT2);
+		glutChangeToMenuEntry(2, "Activar luz 2", E2);
+		break;
+	case D3:
+		glDisable(GL_LIGHT3);
+		glutChangeToMenuEntry(3, "Activar luz 3", E3);
 		break;
 	}
 
 	luzAmbiental();
 	luzUno();
+	luzDos();
 	luzTres();
 
 	glutPostRedisplay();
@@ -198,60 +358,44 @@ void menuFiguras(int opcion)
 //Creacion de menu y submenus
 void creacionMenu(void)
 {
-	int menuFIGURA, menuFONDO, menuCOLOR, menuLUCES, menuMain;
-	int on, off;
+	int menuFigura, menuBG, menuColor, menuMain;
+	int lightControl;
 
-	//----------------------------------------------------------
+	menuFigura = glutCreateMenu(menuFiguras);
+	glutAddMenuEntry("Cubo", F1);
+	glutAddMenuEntry("Cono", F2);
+	glutAddMenuEntry("Esfera", F3);
+	glutAddMenuEntry("Tetera", F4);
+	glutAddMenuEntry("Toroide", F5);
+	glutAddMenuEntry("Icosaedro", F6);
+	glutAddMenuEntry("Dodecaedro", F7);
+	glutAddMenuEntry("Octaedro", F8);
+	glutAddMenuEntry("Tetraedro", F9);
 
-	// NIVEL 2 DE SUB-MENU PARA TIPO DE FIGURA
-	menuFIGURA = glutCreateMenu(menuFiguras);
-	glutAddMenuEntry("Cube", F1);
-	/*
-        AQUI VAN LAS OPCIONES
-    */
+	menuBG = glutCreateMenu(menuFiguras);
+	glutAddMenuEntry("Blanco", C1);
+	glutAddMenuEntry("Negro", C2);
 
-	//----------------------------------------------------------
+	menuColor = glutCreateMenu(menuFiguras);
+	glutAddMenuEntry("Rojo", C3);
+	glutAddMenuEntry("Verde", C4);
+	glutAddMenuEntry("Azul", C5);
+	glutAddMenuEntry("Amarillo", C6);
+	glutAddMenuEntry("Magenta", C7);
+	glutAddMenuEntry("Cian", C8);
+	glutAddMenuEntry("Gris", C9);
 
-	// NUVEL 2 DE SUB-MENU PARA COLOR DE FONDO
-	menuFONDO = glutCreateMenu(menuFiguras);
-	/*
-        AQUI VAN LAS OPCIONES
-    */
-
-	//----------------------------------------------------------
-
-	// NIVEL 3 DE SUB-MENUS PARA ACTIVAR LUCES
-	on = glutCreateMenu(menuFiguras);
-	/*
-            AQUI VAN LAS OPCIONES
-        */
-
-	// NIVEL 3 DE SUB-MENUS PARA DESACTIVAR LUCES
-	off = glutCreateMenu(menuFiguras);
-	/*
-            AQUI VAN LAS OPCIONES
-        */
-
-	// NIVEL 2 DE SU-MENUS
-	menuLUCES = glutCreateMenu(menuFiguras);
-	/*
-        AQUI VAN LOS SUB-MENUS DE NIVEL 3
-    */
-
-	//----------------------------------------------------------
-
-	// NIVEL 2 DE SUB-MENUS
-	menuCOLOR = glutCreateMenu(menuFiguras);
-	/*
-        AQUI VAN LAS OPCIONES
-    */
+	lightControl = glutCreateMenu(menuFiguras);
+	glutAddMenuEntry("Activar luz 1", E1);
+	glutAddMenuEntry("Activar luz 2", E2);
+	glutAddMenuEntry("Activar luz 3", E3);
 
 	// MENU PRINCIPAL (NIVEL 1)
 	menuMain = glutCreateMenu(menuFiguras);
-	glutAddSubMenu("Figura 3D", menuFIGURA);
-	/*
-        AQUI VAN LOS SUB-MENUS DE NIVEL 2
-    */
+	glutAddSubMenu("Figura 3D", menuFigura);
+	glutAddSubMenu("Color de Fondo", menuBG);
+	glutAddSubMenu("Color de Figura", menuColor);
+	glutAddSubMenu("Activar/Desactivar Luces", lightControl);
 
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
@@ -285,21 +429,34 @@ void ControlTeclado(int Key, int x, int y)
 	glutPostRedisplay();
 }
 
+void hitkey(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27: // Escape key
+		glutDestroyWindow(window);
+		exit(0);
+		break;
+	}
+	glutPostRedisplay();
+}
+
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(10, 10);
-	glutCreateWindow("Figuras 3D");
+	glutInitWindowPosition(100, 100);
+	window = glutCreateWindow("Figuras 3D");
 	init();
 	glutDisplayFunc(display);
 	glutSpecialFunc(ControlTeclado);
+	glutKeyboardFunc(hitkey);
 	glutMouseFunc(onMouse);
 	glutMotionFunc(onMotion);
 	creacionMenu();
+	//system("afplay sound.wav &");
 	glutMainLoop();
-
 	return 0;
 }
