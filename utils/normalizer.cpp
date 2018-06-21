f977eb39d8ad2c6d6968e6659defb24e994263d9
Marco A. Rojas <contact@marcorojas.me>
2018-06-21 18:13:21 -0500

    Using iostream instead of stdio

--- utils/normalizer.cpp
+++ utils/normalizer.cpp
@@ -1,5 +1,5 @@
 #define _USE_MATH_DEFINES
-#include <stdio.h>
+#include <iostream>
 #include <math.h>
 #include "normalizer.h"
 
@@ -20,11 +20,23 @@ void Normalizer ::normalize(float *v)
 float *Normalizer ::crossProduct(float *a, float *b)
 {
 	float result[] = {
-			a[1] * b[2] - a[2] * b[1],
-			a[0] * b[2] - a[2] * b[0],
-			a[0] * b[1] - a[1] * b[0]};
+			a[1] * b[2] - a[2] * b[1], // X
+			a[0] * b[2] - a[2] * b[0], // y
+			a[0] * b[1] - a[1] * b[0]};// z
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\n" << result[i];
+	}
+
+	cout << "\n\nAfter Normalize\n";
 
 	this->normalize(result);
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\n" << result[i];
+	}
 
 	return result;
 }
